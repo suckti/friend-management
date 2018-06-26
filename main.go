6f198e4f806e524edcae46e8f3e9ef9fc9b5f344
Wira Sakti <wirasakti@Wiras-MacBook-Pro.local>
2018-06-26 15:44:28 +0700

    friend request logic. - add email in json body as user in mongodb collections. The collections consist of 4 key (email,friend,block,subscribe) - Check condition if they already friend or the email is blocking - define helper to check friend already exist in map slice

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"github.com/gin-gonic/gin"
 	"google.golang.org/appengine"
 	"gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2/bson"
 )
 
 type User struct {
@@ -71,7 +72,80 @@ func home(c *gin.Context) {
 }
 
 func friendRequest(c *gin.Context) {
+	var f Friend
+	status := true
+	message := ""
 
+	//init db
+	sess := getDB()
+	defer sess.Close()
+	db := sess.DB("imd").C("users")
+
+	//get request from post
+	c.BindJSON(&f)
+
+	//get value in friend array
+	u1 := f.Friends[0]
+	u2 := f.Friends[1]
+	block := false
+
+	//first user
+	user1 := User{}
+	err := db.Find(bson.M{"email": u1}).One(&user1)
+	if err != nil { //if not found
+		var friend []string
+
+		friend = append(friend, u2)
+		db.Insert(&User{u1, friend, []string{}, []string{}})
+
+	} else { //update friend if already exist
+		exist := checkSliceExist(user1.Friends, u2) //check if already friend
+		if exist == false {
+			block := checkSliceExist(user1.Block, u2) //check if blocked
+			if block == false {
+				friend := append(user1.Friends, u2)
+				db.Update(&user1, &User{u1, friend, user1.Block, user1.Subscribe})
+			} else {
+				status = false
+				block = true
+				message = "This two user can't be friend"
+			}
+		} else {
+			status = false
+			message = "This two user already be friends"
+		}
+	}
+
+	//second user
+	user2 := User{}
+	err = db.Find(bson.M{"email": u2}).One(&user2)
+	if err != nil { //if not found
+		var friend []string
+
+		friend = append(friend, u1)
+		db.Insert(&User{u2, friend, []string{}, []string{}})
+
+	} else if block == false { //update friend if already exist and not blocked
+		exist := checkSliceExist(user2.Friends, u1)
+		if exist == false {
+			friend := append(user2.Friends, u1)
+			db.Update(&user2, &User{u2, friend, user2.Block, user2.Subscribe})
+		} else {
+			status = false
+			message = "This two user already be friends"
+		}
+	}
+
+	if status == true {
+		c.JSON(200, gin.H{
+			"success": status,
+		})
+	} else {
+		c.JSON(200, gin.H{
+			"status":  status,
+			"message": message,
+		})
+	}
 }
 
 func friendList(c *gin.Context) {
@@ -92,4 +166,16 @@ func block(c *gin.Context) {
 
 func notification(c *gin.Context) {
 
+}
+
+// HELPER
+func checkSliceExist(elements []string, email string) bool {
+	// Create a map of all unique elements.
+	for v := range elements {
+		if elements[v] == email {
+			return true
+		}
+	}
+
+	return false
 }
