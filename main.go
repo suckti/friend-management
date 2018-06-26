aafd279681b25d2820e3e6a34f43069ad97fa2c8
Wira Sakti <wirasakti@Wiras-MacBook-Pro.local>
2018-06-26 15:37:01 +0700

    define function in api. Http request using Gin Gionic context. Define 6 logic endpoint and 1 endpoint for home access.

--- main.go
+++ main.go
@@ -1,5 +1,12 @@
 package main
 
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"google.golang.org/appengine"
+)
+
 type User struct {
 	Email     string   `json:"email"`
 	Friends   []string `json:"friends"`
@@ -27,5 +34,50 @@ type Person struct {
 }
 
 func main() {
+	r := gin.Default()
+
+	r.GET("/", home)
+
+	r.POST("/friend_request", friendRequest)
+	r.POST("/friend_list", friendList)
+	r.POST("/friend_common", friendCommon)
+	r.POST("/subscribe", subscribe)
+	r.POST("/block", block)
+	r.POST("/notification", notification)
+
+	// r.Run() //this code is for local testing run
+
+	//below code using for google cloud app engine run
+	http.Handle("/", r)
+	appengine.Main()
+}
+
+func home(c *gin.Context) {
+	c.JSON(200, gin.H{
+		"message": "Friend management api",
+	})
+}
+
+func friendRequest(c *gin.Context) {
+
+}
+
+func friendList(c *gin.Context) {
+
+}
+
+func friendCommon(c *gin.Context) {
+
+}
+
+func subscribe(c *gin.Context) {
+
+}
+
+func block(c *gin.Context) {
+
+}
+
+func notification(c *gin.Context) {
 
 }
