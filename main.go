07f57799c8988c776cae51420ff3d6bb64bab3d8
Wira Sakti <wirasakti@Wiras-MacBook-Pro.local>
2018-06-26 15:33:02 +0700

    define struct to retrieve request from json body

--- main.go
+++ main.go
@@ -1,5 +1,31 @@
 package main
 
-func main(){
-	
+type User struct {
+	Email     string   `json:"email"`
+	Friends   []string `json:"friends"`
+	Block     []string `json:"block"`
+	Subscribe []string `json:"subscribe"`
+}
+
+type Friend struct {
+	Friends []string
+}
+
+type Subscribe struct {
+	Requestor string
+	Target    string
+}
+
+type Notification struct {
+	Sender string
+	Text   string
+}
+
+type Person struct {
+	Name  string
+	Phone string
+}
+
+func main() {
+
 }
